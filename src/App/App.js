2be2ca6d1fb9825685e062de17c0912ef794f336
Jamie <phillipsja97@gmail.com>
2020-01-04 09:42:49 -0600

    setup complete

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+     <button className="btn btn-danger">Hi</button>
+    </div>
+  );
+}
+
+export default App;
